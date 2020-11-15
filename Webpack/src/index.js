525602e593fcdf17de46de3ba77709c28de01a76
haithem khalifa <v-haikha@microsoft.com>
2020-11-15 17:09:24 +0100

    Add rxjs to wabpack project

--- Webpack/src/index.js
+++ Webpack/src/index.js
@@ -3,3 +3,12 @@ import 'bootstrap';
 import './styles/styles.scss';
 
 $('#demo').html('Bonjour :)');
+
+
+import { of } from 'rxjs';
+import { every, tap } from 'rxjs/operators';
+const numbers = of(1, 2, 3, 4, 5, 45);  
+numbers.pipe(
+    tap(x=> console.log('emit => ',x)),
+    every(x  => x< 10 )
+    ).subscribe(v => console.log(v));
